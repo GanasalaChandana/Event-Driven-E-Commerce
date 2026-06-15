3f9f1981ebd4436acd23f22613141f3a28441ab1
GanasalaChandana <chandanacherry869@gmail.com>
2026-06-15 11:41:32 -0500

    fix: remove version field from compose, simplify init-db.sql

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -5,9 +5,3 @@ CREATE DATABASE users_db;
 CREATE DATABASE products_db;
 CREATE DATABASE orders_db;
 CREATE DATABASE inventory_db;
-
--- Grant all privileges to the shopflow user
-GRANT ALL PRIVILEGES ON DATABASE users_db TO shopflow;
-GRANT ALL PRIVILEGES ON DATABASE products_db TO shopflow;
-GRANT ALL PRIVILEGES ON DATABASE orders_db TO shopflow;
-GRANT ALL PRIVILEGES ON DATABASE inventory_db TO shopflow;
