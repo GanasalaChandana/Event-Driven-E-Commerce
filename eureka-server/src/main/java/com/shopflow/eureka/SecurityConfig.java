34c99d1c29f949b4673b25ce0d24d404274f7e89
GanasalaChandana <chandanacherry869@gmail.com>
2026-06-15 18:29:32 -0500

    fix: explicitly add spring-security to eureka-server and remove conditional guard

--- eureka-server/src/main/java/com/shopflow/eureka/SecurityConfig.java
+++ eureka-server/src/main/java/com/shopflow/eureka/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.shopflow.eureka;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,7 +8,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@ConditionalOnClass(name = "org.springframework.security.web.SecurityFilterChain")
 public class SecurityConfig {
 
     @Bean
